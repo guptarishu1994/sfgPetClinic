01768b8a0d3f5163495c5da348662d18e6149417
rgupta335 <rgupta335@IN-PF1PD0EQ>
2022-01-27 13:30:22 +0530

    initial sfg pet clinic

---
+++ sfgPetClinic_2/src/test/java/com/rish/sfgPetClinic/SfgPetClinic2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rish.sfgPetClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinic2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
