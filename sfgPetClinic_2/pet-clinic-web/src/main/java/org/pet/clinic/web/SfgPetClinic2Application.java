0128d5c0e9081676f5cf33ae4f8fae2a779feac4
rgupta335 <rgupta335@IN-PF1PD0EQ>
2022-01-28 00:13:18 +0530

    Build multi module project

--- sfgPetClinic_2/pet-clinic-web/src/main/java/org/pet/clinic/web/SfgPetClinic2Application.java
+++ sfgPetClinic_2/pet-clinic-web/src/main/java/org/pet/clinic/web/SfgPetClinic2Application.java
@@ -1,4 +1,4 @@
-package com.rish.sfgPetClinic;
+package org.pet.clinic.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- sfgPetClinic_2/pet-clinic-web/src/test/java/org/pet/clinic/web/SfgPetClinic2ApplicationTests.java
+++ sfgPetClinic_2/pet-clinic-web/src/test/java/org/pet/clinic/web/SfgPetClinic2ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rish.sfgPetClinic;
+package org.pet.clinic.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
